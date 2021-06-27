b54056e9b3878a052a9118df4c6ddf46bfacc9d4
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2021-06-27 14:01:17 -0700

    first

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// main.rs
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
